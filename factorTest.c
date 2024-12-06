dd2218f9f07d6dd58223ac7e5de133d8fc523766
asomkyriakos <159484676+asomkyriakos@users.noreply.github.com>
2024-12-06 09:39:30 +0200

    Update factorTest.c

--- factorTest.c
+++ factorTest.c
@@ -1,57 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-
-unsigned long long int C3(unsigned long long int n){
-    if (n<10) return n;
-    return n%10 + C3(n/10);
-}
-
-
-
-int prime(unsigned long long int k){
-    int s = 1;
-    for(unsigned long long int i = 2 ; i < k ; i++){
-        if((k % i) == 0){
-            s++;
-        } 
-    }
-    if(s==1){
-        return 0;
-    }else {
-        return 1;
-    }
-}
-// αθροισμα ψηγιων /3 != 0
-unsigned long long int semiprime1(unsigned long long int a){
-    unsigned long long int k;
-    unsigned long long int s1 = 2;
+unsigned long long int semiprime1(uint64_t a){
+    uint64_t k =3;
+    uint64_t s1 = 0;
+    uint64_t i =1;
+    int flag = 0;
     
+
     if((a % 2) == 0){
-        s1 =2;
+        s1 = 2;
         return s1;
-    }else{
-        for( k = 3 ; k*k <= a ; k = k+2  ){
-            if((k % 10 != 5) || (k == 5)){
-                if(C3(k) % 3 != 0 || k == 3){
-                    if(a % k == 0){
-                        if(prime(k) == 0){
-                            if((a % k) == 0 ){
-                                s1 = k;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
+    }
+    if((a % 3) == 0){
+        s1 = 3;
+        return s1;
+    }
+    while(flag == 0 && k*k <= a ){
+
+        // mod 5 ? mod 3?
+        if(a % k == 0){
+            s1 =k;
+            flag = 1;
+            return s1;
+        }else{
+            k =3+(i*2);
+            i = i+1;
         }
-        return s1;
+        if(a % k == 0){
+            s1 =k;
+            flag =1;
+            return s1;
+        }else{
+            k =3+(i*2);
+            i =i+2;
+        }
     }
     return s1;
 }
-
-
 
 
 int main(int argc , char * argv[]){
@@ -61,15 +49,16 @@ int main(int argc , char * argv[]){
         return 1;
     }
     long long int test;
-    unsigned long long int a,s1,s2;
+    uint64_t a,s1,s2;
     char *endptr1;
     test = strtoll(argv[1] , &endptr1 , 10);
-    a = strtoll(argv[1] , &endptr1 , 10);
-    if(test < 0){
+    if(test <= 0){
+        printf("Usage: %s <semiprime>\n" , argv[0]);
         return 1;
     }
+    a = strtoll(argv[1] , &endptr1 , 10);
     s1 = semiprime1(a);
     s2 = a/s1;
-    printf("Factors: %llu %llu\n" , s1 ,s2);
+    printf("Factors: %lu %lu\n" , s1 ,s2);
     return 0;
 }
